206385079e98d459a7d65382d91bc2b0ddd08f36
Emiliano Papasidero <e.papasidero21@gmail.com>
2020-02-27 01:09:28 -0300

    Final Version

---
+++ src/ahorcado/run/Run.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahorcado.run;
+
+import ahorcado.controlador.Controlador;
+
+/**
+ *
+ * @author Comisión #60 C.F.P. n°11
+ */
+public class Run {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {               
+                Controlador c = new Controlador();
+            }
+        });
+    }
+    
+}
